ac918225d13c8d50c1b18f6dbe5b4bc356be6298
新井寿明 <toshipy@araitoshiakis-MacBook-Pro.local>
2022-09-04 22:18:45 +0900

    stripe not found

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,9 +5,9 @@
  * to customize this controller
  */
 
-const stripe = require("stripe")(
-  "sk_test_51Ldx6qEqFs9YAZh67itbK3EXvzukHSPxUpwKWr4BC9LheOHhMAmHwU6tJPv5ZfsdJDuuvujWsH685P458RZSP9Ld00WZwFv7ZC"
-);
+// const stripe = require("stripe")(
+//   "sk_test_51Ldx6qEqFs9YAZh67itbK3EXvzukHSPxUpwKWr4BC9LheOHhMAmHwU6tJPv5ZfsdJDuuvujWsH685P458RZSP9Ld00WZwFv7ZC"
+// );
 
 module.exports = {
   /**
@@ -16,26 +16,26 @@ module.exports = {
    * @return {Object}
    */
 
-  create: async (ctx) => {
-    const { address, amount, dishes, token } = JSON.parse(ctx.request.body);
-    const stripeAmount = Math.floor(amount * 100); //5.95 ⇨ 5
-    // charge on stripe
-    const charge = await stripe.charges.create({
-      amount: stripeAmount,
-      currency: "jpy",
-      description: `Order ${new Date()} by ${ctx.state.user._id}`,
-      source: token,
-    });
+  // create: async (ctx) => {
+  //   const { address, amount, dishes, token } = JSON.parse(ctx.request.body);
+  //   const stripeAmount = Math.floor(amount * 100); //5.95 ⇨ 5
+  //   // charge on stripe
+  //   const charge = await stripe.charges.create({
+  //     amount: stripeAmount,
+  //     currency: "jpy",
+  //     description: `Order ${new Date()} by ${ctx.state.user._id}`,
+  //     source: token,
+  //   });
 
-    //データベースに注文を登録する
-    const order = await strapi.services.order.create({
-      user: ctx.state.user.id,
-      charge_id: charge.id,
-      amount: stripeAmount,
-      address,
-      dishes,
-    });
+  //   //データベースに注文を登録する
+  //   const order = await strapi.services.order.create({
+  //     user: ctx.state.user.id,
+  //     charge_id: charge.id,
+  //     amount: stripeAmount,
+  //     address,
+  //     dishes,
+  //   });
 
-    return order;
-  },
+  //   return order;
+  // },
 };
