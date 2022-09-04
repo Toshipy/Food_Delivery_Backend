0a2868fe001bda7dae8fd6dab8401a30ca8bf886
新井寿明 <toshipy@araitoshiakis-MacBook-Pro.local>
2022-09-04 20:03:43 +0900

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bc277274d6f54a2997685cc26139875a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8938cc8b-f95d-4116-b097-0f3d6fe48365'
+};
